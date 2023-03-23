602d9b0409521301f38559fdabf28d282d7608e9
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-03-23 14:11:39 -0700

    a

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,20 +4,19 @@
  * more_numbers - print more numbers
  */
 
+void more_numbers(void)
 {
-    int y;
-    int z;
+	int i, j;
 
-    while (z = 0; z < 10; z++)
-    {
-        while (y = 0 ; y <= 14; y ++)
-        {
-            putchar(y);
-            
-        }
-        y -= 15;
-        putchar('\n');
-        
-    }
-    
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+				_putchar('1');
+			_putchar (j % 10 + '0');
+		}
+		_putchar('\n');
+	}
 }
+
