f66543cf473e7d7b2016e57c1cc776adff7eacf7
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-03-27 16:59:50 -0700

    a

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,12 +5,15 @@
  * Return: length
  */
 
-int _strlen(char *s) {
+int _strlen(char *s)
+{
 	int len = 0;
-	while (*s != '\0') {
+
+	while (*s != '\0')
+	{
 		len++;
 		s++;
 	}
-	return len;
+	return (len);
 }
 
