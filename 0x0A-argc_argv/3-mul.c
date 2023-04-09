14458e4d6bfa3fcb5e892ce1874ca0db033801fb
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-04-09 15:06:36 -0700

    a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -59,18 +59,16 @@ int main(int argc, char *argv[])
 	int n2;
 	int res;
 
-	n1 = _atoi(argv[1]);
-	n2 = _atoi(argv[2]);
-	res = n1 * n2;
-
-	if (argc == 3)
-	{
-		printf("%d\n", res);
-		return (0);
-	}
-	else
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	n1 = _atoi(argv[1]);
+	n2 = _atoi(argv[2]);
+	res = n1 * n2;
+
+	printf("%d\n", res);
+	return (0);
 }
