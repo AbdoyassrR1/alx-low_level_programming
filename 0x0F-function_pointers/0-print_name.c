4ac8a956e75173d3fe760cff981d7d7f9992a72c
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-04-20 03:01:42 +0200

    a

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * print_name - a function that prints a name.
+ *@name: str to add
+ *@f:ptr to function.
+ *  Return: nothing
+ */
+
+ void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
