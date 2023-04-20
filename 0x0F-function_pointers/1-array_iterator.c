959e8f734968e3759dcb052dbff8c57fe6cbbe1d
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-04-20 04:06:29 +0200

    a

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
- * array_iterator -  a function that executes a function 
+ *array_iterator -  a function that executes a function 
  *@array:QQQ
  *@size:QQQ
  *@action:QQQ
