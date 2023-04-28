0903398f6ed158018a3bd8dadde3a2d2b905d644
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-04-28 02:10:47 +0200

    a

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,7 +3,7 @@
 void _constructor(void) __attribute__ ((constructor));
 
 /**
- * _constructor - executes before main()
+ * _constructor - execute before main()
  * Return: void
  */
 void _constructor(void)
