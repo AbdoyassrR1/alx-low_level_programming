3a12f5d5a342dd205c084350500d3dc82149880e
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-04-29 00:39:33 +0200

    a

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - function that prints all the elements
+ * @h: linked list
+ *
+ * Return: number of nodes.
+*/
+
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+
+	while (h == NULL)
+	{
+		printf("%d\n", (*h).n);
+		i++;
+		h = (*h).next;
+	}
+	return (i);
+}
