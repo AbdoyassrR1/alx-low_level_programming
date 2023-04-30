60dc040a4e28d07c630c487a9388a7506c1df30d
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-04-30 15:02:24 +0200

    a

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -8,12 +8,12 @@
 
 void free_listint(listint_t *head)
 {
-    listint_t *ptr;
+	listint_t *ptr;
 
-    while (head)
-    {
-        ptr = (*head).next;
-        free(head);
-        head = ptr;
-    }
+	while (head)
+	{
+		ptr = (*head).next;
+		free(head);
+		head = ptr;
+	}
 }
