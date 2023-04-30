45ac71161f8ba32c263e69779972a6a05a1d60a1
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-04-30 18:24:14 +0200

    a

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,12 +10,9 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int count;
+	unsigned int count = 0;
 
-	count = 0;
-	listint_t *ptr;
-
-	ptr = head;
+	listint_t *ptr = head;
 
 	while (ptr && count < index)
 	{
