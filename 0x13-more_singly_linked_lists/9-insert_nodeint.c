1f35dd5d0b9ae46171fc9176791a6cef345b3d57
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-05-01 23:05:43 +0200

    a

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,7 +11,7 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int count = 0;
+	unsigned int count;
 	listint_t *new;
 	listint_t *ptr = *head;
 
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (new);
 	}
 
-	for (; ptr && count < idx; count++)
+	for (count = 0; ptr && count < idx; count++)
 	{
 		if (count == idx - 1)
 		{
@@ -40,6 +40,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			ptr = ptr->next;
 		}
 	}
-
 	return (NULL);
 }
