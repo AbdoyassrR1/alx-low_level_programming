d58f9fc9aff66090884b29d609e094a51b22f36f
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-05-01 22:26:41 +0200

    a

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+
+/**
+ * sum_listint - function that returns the sum of all the data (n)
+ * @head: pointer to the first element in the list.
+ * Return: sum of data (n) or 0 if empty list.
+*/
+
+
+
+int sum_listint(listint_t *head)
+{
+
+	listint_t *ptr = head;
+	int sum = 0;
+
+
+	if (!head)
+	{
+		return (0);
+	}
+
+	while (head)
+	{
+		sum += (*ptr).n;
+		ptr = (*ptr).next;
+	}
+
+	return (sum);
+}
