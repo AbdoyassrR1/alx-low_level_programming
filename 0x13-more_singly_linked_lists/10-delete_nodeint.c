a571c8f3d14f52e736ccc18db1bc8087a6f2e929
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-05-02 00:00:07 +0200

    a

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -36,7 +36,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 
 	current = (*ptr).next;
-	(*ptr).next = (*current).next
+	(*ptr).next = (*current).next;
 	free(current);
 
 	return (1);
