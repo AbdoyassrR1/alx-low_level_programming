e47aee21f894c1ecdecdd2fa9e5c73401771291d
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-05-01 23:35:53 +0200

    a

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -13,7 +13,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int count = 0;
 	listint_t *ptr = *head;
-	listint_t *currunt;
+	listint_t *currunt = NULL;
 
 
 	if (!head)
