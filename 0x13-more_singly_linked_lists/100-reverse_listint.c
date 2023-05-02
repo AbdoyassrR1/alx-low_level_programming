c2fca667dd621d5f09ba10302342a84a4f61a4bf
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-05-02 17:23:38 +0200

    a

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -6,24 +6,18 @@
  * Return: a pointer to the first node of the reversed list
 */
 
-// first we need to ptr to point to the sec node of the list
-// sec update the first node to NULL
-// set the temp to point to first node
-// move the head to sec node 
-// end the loop when head == NULL
-
 listint_t *reverse_listint(listint_t **head)
 {
-    listint_t *next = NULL;
-    listint_t *prev = NULL;
+	listint_t *next = NULL;
+	listint_t *prev = NULL;
 
-    while (head != NULL)
-    {
-        next = (*head)->next;
-        (*head)->next = prev;
-        prev = *head;
-        *head = next;
-    }
-    *head = prev;
-    return (*head); 
+	while (head != NULL)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+	return (*head);
 }
