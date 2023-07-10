cba9241e0bfabe867123245aee62cdae6ca18380
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-07-11 00:28:58 +0200

    fix bugs

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	int w;
 	int length;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
 	fd = open(filename, O_CREAT, O_WRONLY, O_TRUNC, 0600);
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	while (text_content)
+	while (text_content != NULL)
 	{
 		for (length = 0; text_content[0];)
 		{
