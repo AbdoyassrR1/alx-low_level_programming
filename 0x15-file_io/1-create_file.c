80ff84531fdd82f4c0a69e8b0ea3c0f4ce8685a6
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-07-11 00:03:56 +0200

    Task 2

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file.
+ * @filename: the name of file to create.
+ * @text_content: a NULL terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t fd;
+	ssize_t w;
+	ssize_t length;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT, O_WRONLY, O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	while (text_content)
+	{
+		for (length = 0; text_content[0];)
+		{
+			length++;
+		}
+	}
+	
+	w = write(fd, text_content, length);
+
+	if (w == -1)
+		return (-1);
+		
+	close(fd);
+	return (1);
+}
