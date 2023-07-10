24525765c7153dff03d377f24012d4738e6646f3
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-07-10 23:27:49 +0200

    fix bug

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,7 +11,7 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fp;
+	size_t fp;
 	char *buffer;
 	size_t r;
 	size_t w;
@@ -34,4 +34,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	free(buffer);
 	close(fp);
 	return (r);
+}
+
+int main(int ac, char **av)
+{
+    ssize_t n;
+
+    if (ac != 2)
+    {
+        dprintf(2, "Usage: %s filename\n", av[0]);
+        exit(1);
+    }
+    n = read_textfile(av[1], 114);
+    printf("\n(printed chars: %li)\n", n);
+    n = read_textfile(av[1], 1024);
+    printf("\n(printed chars: %li)\n", n);
+    return (0);
 }
