ae949c6f6ef62adda003ef98fe856443cff721e8
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-07-11 01:01:35 +0200

    fix betty

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,19 +7,19 @@
  * @filename: the name of the file.
  * @text_content: NULL terminated string to add at the end of the file.
  *
- *Return: 1 on success, -1 on failure 
+ *Return: 1 on success, -1 on failure
 */
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd;
-    int length = 0;
-    int w;
+	int fd;
+	int length = 0;
+	int w;
 
-    if (filename == NULL)
-        return (-1);
+	if (filename == NULL)
+		return (-1);
 
-    if (text_content != NULL)
+	if (text_content != NULL)
 	{
 		for (length = 0; text_content[length];)
 		{
@@ -27,14 +27,14 @@ int append_text_to_file(const char *filename, char *text_content)
 		}
 	}
 
-    fd = open(filename, O_WRONLY | O_APPEND);
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-    w = write(fd, text_content, length);
+	w = write(fd, text_content, length);
 
-    if (fd == -1 || w == -1)
-        return (-1);
+	if (fd == -1 || w == -1)
+		return (-1);
 
-    close(fd);
+	close(fd);
 
-    return (1);
+	return (1);
 }
