dc0d99923276299bc1e5207afd116354b7d2784c
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-07-11 00:42:19 +0200

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 
-	while (text_content != NULL)
+	if (text_content != NULL)
 	{
 		for (length = 0; text_content[length];)
 		{
