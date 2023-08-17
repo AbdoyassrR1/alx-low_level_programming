a5ed74c9140d42c3aa42e8d8c8276f3979b7e384
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-08-17 21:17:05 +0200

    Free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - free linked list
+ * @head: ptr to the first element in the list
+ *
+ * Return: no thing
+*/
+void free_dlistint(dlistint_t *head)
+{
+    if (head == NULL)
+    {
+        return 0;
+    }
+    while (head != NULL)
+    {
+        head = head->next;
+        free(head);
+    }
+
+    
+}
