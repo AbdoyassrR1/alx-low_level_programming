10bdcb3edc5c40267804f4427508f12349c2ac7f
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-09-09 05:15:51 +0300

    djb2

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_djb2 - function implementing the djb2 algorithm.
+ * @str: value
+ *
+ * Return: the hash number
+*/
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+    unsigned long int h;
+	int count;
+
+	h = 5381;
+	while ((count = *str++))
+	{
+		h = ((h << 5) + h) + count;
+	}
+	return (h);
+}
