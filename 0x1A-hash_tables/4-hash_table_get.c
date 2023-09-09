822d6520a0a0fcd872b8353cab8392d23bf34be2
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-09-09 05:28:37 +0300

    retrieve

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_get - function that retrieves a value associated with a key.
+ * @ht: hash tabel
+ * @key: the key
+ *
+ * Return: the value of the key , otherwise NULL
+*/
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+    unsigned long int i = 0;
+	hash_node_t *temp;
+
+	if (!ht || !key)
+	{
+		return (NULL);
+	}
+
+	i = key_index((const unsigned char *)key, ht->size);
+	temp = ht->array[i];
+
+	while (temp)
+	{
+		if (strcmp(temp->key, key) == 0)
+		{
+			return (temp->value);
+		}
+		temp = temp->next;
+	}
+	return (NULL);
+}
