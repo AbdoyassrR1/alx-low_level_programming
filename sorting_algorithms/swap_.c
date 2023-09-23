2cce696a301e4b5437c9d176e52df314a584f0ec
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-09-23 07:24:56 +0300

    add swap func

--- sorting_algorithms/swap_.c
+++ sorting_algorithms/swap_.c
@@ -1,13 +1,13 @@
 #include "sort.h"
 
 /**
- * swap - swap to element
+ * _swap - swap to element
  * @a: the first element
  * @b: the second element
  *
  * Return: no thing
 */
-void swap(int *a, int *b)
+void _swap(int *a, int *b)
 {
 	int temp = *a;
 	*a = *b;
