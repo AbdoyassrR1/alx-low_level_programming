1a7278283d4aff8db1b9799448afdb642e8ac563
Abdo Yasser <abdoyasser2018a@gmail.com>
2023-10-26 05:50:39 +0300

    Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
+
+def island_perimeter(grid):
+    """ Returns the perimeter of an island described in grid """
+
+    width = len(grid[0])
+    height = len(grid)
+    edge = 0
+    size = 0
+
+    for x in range(height):
+
+        for y in range(width):
+
+            if grid[x][y] == 1:
+                size += 1
+                if (y > 0 and grid[x][y - 1] == 1):
+                    edge += 1
+                if (x > 0 and grid[x - 1][y] == 1):
+                    edge += 1
+
+    return size * 4 - edge * 2
