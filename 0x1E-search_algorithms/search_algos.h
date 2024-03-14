4092adfc0503f0f04b47ab3444ec53b52b255fbf
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-03-14 17:15:38 +0200

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,6 @@ int linear_search(int *array, size_t size, int value);
 void print_arr_at_index(int *array, int low, int high);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-
+int interpolation_search(int *array, size_t size, int value);
 
 #endif
