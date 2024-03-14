1a0dbd0ed85f2e3c382771b347bb762858443b19
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-03-14 03:45:43 +0200

    update header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,6 +7,7 @@
 int linear_search(int *array, size_t size, int value);
 void print_arr_at_index(int *array, int low, int high);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 
 #endif
