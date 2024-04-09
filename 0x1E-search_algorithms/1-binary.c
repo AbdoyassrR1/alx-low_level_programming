f046210095147a46f8bfdb4279362ef234cadc22
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-04-09 07:06:57 +0200

    fix betty style

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,20 +12,16 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	size_t low;
-	size_t high;
+	size_t low, high, index;
 	int middle;
-	size_t index;
 
 	if (!array || !value)
 	{
 		return (-1);
 	}
-
 	low = 0;
 	high = size - 1;
 	middle = (low + high) / 2;
-
 	while (high >= low)
 	{
 		printf("Searching in array: ");
@@ -40,25 +36,20 @@ int binary_search(int *array, size_t size, int value)
 				printf("%d, ", array[index]);
 			}
 		}
-
 		if (value == array[middle])
 		{
 			return (middle);
 		}
-
 		if (value > array[middle])
 		{
 			low = middle + 1;
 			middle = (low + high) / 2;
 		}
-
 		else if (value < array[middle])
 		{
 			high = middle - 1;
 			middle = (low + high) / 2;
 		}
 	}
-
 	return (-1);
-
 }
