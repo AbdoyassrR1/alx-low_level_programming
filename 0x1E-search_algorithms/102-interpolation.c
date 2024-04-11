40613d035c9b40d2a910b8594b1285785fab3087
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-04-11 11:01:20 +0200

    fix betty style

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -20,7 +20,8 @@ int interpolation_search(int *array, size_t size, int value)
 	}
 	low = 0;
 	high = size - 1;
-	pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+	pos = low + (((double)(high - low) / (array[high] - array[low]))
+	* (value - array[low]));
 	while (high >= low)
 	{
 		if (pos > size - 1)
@@ -36,12 +37,14 @@ int interpolation_search(int *array, size_t size, int value)
 		else if (value > array[pos])
 		{
 			low = pos + 1;
-			pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+			pos = low + (((double)(high - low) / (array[high] - array[low]))
+			* (value - array[low]));
 		}
 		else if (value < array[pos])
 		{
 			high = pos - 1;
-			pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+			pos = low + (((double)(high - low) / (array[high] - array[low]))
+			* (value - array[low]));
 		}
 	}
 
