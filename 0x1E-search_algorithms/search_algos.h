250c92a369d1447c1895ce5ed9ad3554e155b408
Abdo Yasser <abdoyasser2018a@gmail.com>
2024-04-12 11:54:02 +0200

    add printing format prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -11,5 +11,7 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int min(int x, int y);
+void printing_format(int *array, size_t low, size_t high);
+
 
 #endif
